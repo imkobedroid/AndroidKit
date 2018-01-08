c8db1c2daa984192039f78fac4b5f9c385ea0b80
dongshihong <imkobedroid@gmail.com>
2018-01-08 14:47:09 +0800

    新增加了圆形自定义控件

--- app/src/main/java/com/amijiaoyu/babybus/android/ui/HomeActivity.java
+++ app/src/main/java/com/amijiaoyu/babybus/android/ui/HomeActivity.java
@@ -1,22 +1,16 @@
 package com.amijiaoyu.babybus.android.ui;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
-import android.view.MenuItem;
-import butterknife.BindView;
+
 import com.amijiaoyu.babybus.android.R;
 import com.amijiaoyu.babybus.android.base.RxActivity;
-import com.amijiaoyu.babybus.android.utils.BottomNavigationViewHelper;
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
+import com.amijiaoyu.babybus.android.weight.CircularStatisticsView;
 
-import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Date:2017/10/16 14:00
@@ -25,31 +19,36 @@ import java.util.List;
  * @author dongshihong
  */
 
-public class HomeActivity extends RxActivity
-    implements BottomNavigationView.OnNavigationItemSelectedListener {
-  @BindView(R.id.view_pager) ViewPager viewPager;
-  @BindView(R.id.navigation) BottomNavigationView navigationView;
-  private HomeFragmentAdapter pagerAdapter;
-  private List<Fragment> fragmentList;
+public class HomeActivity extends RxActivity{
+    @BindView(R.id.cs_view)CircularStatisticsView statisticsView;
+
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     initView();
   }
 
-  private void initView() {
-    navigationView.setOnNavigationItemSelectedListener(this);
-    fragmentList = new ArrayList<>();
-    fragmentList.add(new HomeFragment());
-    fragmentList.add(new HomeTestFragment());
-    fragmentList.add(new HomeFragment());
-    fragmentList.add(new HomeTestFragment());
-    BottomNavigationViewHelper.disableShiftMode(navigationView);
-    pagerAdapter = new HomeFragmentAdapter(getSupportFragmentManager(), fragmentList);
-    viewPager.setAdapter(pagerAdapter);
-  }
+    private void initView() {
+        // 添加的是颜色
+        List<Integer> colorList = new ArrayList<>();
+        colorList.add(R.color.alert_default_error_background);
+        colorList.add(R.color.support_ui_blue);
+        colorList.add(R.color.alerter_default_success_background);
+        colorList.add(R.color.blue_btn_bg_color);
 
-  @Override protected int getLayout() {
+        //  添加的是百分比
+        List<Float> rateList = new ArrayList<>();
+        rateList.add(10f);
+        rateList.add(5f);
+        rateList.add(45f);
+        rateList.add(40f);
+        statisticsView.setShow(colorList, rateList,true,false);
+
+
+    }
+
+
+    @Override protected int getLayout() {
     return R.layout.activity_home;
   }
 
@@ -57,11 +56,4 @@ public class HomeActivity extends RxActivity
   }
 
 
-  @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-    switch (item.getItemId()) {
-
-      default:
-    }
-    return true;
-  }
 }
