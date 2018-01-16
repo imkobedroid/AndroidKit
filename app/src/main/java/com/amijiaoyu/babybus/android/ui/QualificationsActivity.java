6587d2e8fbfa0e64966768591c0bfa9b0837037e
dongshihong <imkobedroid@gmail.com>
2018-01-16 11:31:35 +0800

    完成了部分界面

---
+++ app/src/main/java/com/amijiaoyu/babybus/android/ui/QualificationsActivity.java
@@ -0,0 +1,43 @@
+package com.amijiaoyu.babybus.android.ui;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import com.amijiaoyu.babybus.android.R;
+import com.amijiaoyu.babybus.android.base.RxActivity;
+
+import butterknife.BindView;
+
+/**
+ * @author moerlong
+ * @date 2018/1/16
+ */
+
+@SuppressLint("Registered")
+public class QualificationsActivity extends RxActivity {
+    @BindView(R.id.tv_toolbar)
+    TextView tvToolbar;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.recycleView)
+    RecyclerView recycleView;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_qualification;
+    }
+
+    @Override
+    protected void initInject() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initToolBar(toolbar, tvToolbar, getString(R.string.match));
+    }
+}
