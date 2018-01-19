77c38225e552d39545922a26f55a26e884e1ce43
droid <379760818@qq.com>
2018-01-19 14:47:24 +0800

    修改了后台返回错误与请求发生错误的统一处理的框架修改

--- app/src/main/java/com/amijiaoyu/babybus/android/model/rx/RxErrorHandler.java
+++ app/src/main/java/com/amijiaoyu/babybus/android/model/rx/RxErrorHandler.java
@@ -4,85 +4,82 @@ import android.accounts.NetworkErrorException;
 import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.amijiaoyu.babybus.android.ui.NoUser;
 import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
-
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
-
 import retrofit2.HttpException;
 import work.wanghao.rxbus2.RxBus;
 
 /**
+ *
  * @author Dsh
  * @date 2017/5/8
  * Use:
  */
 public class RxErrorHandler {
 
-    private static final int NO_FIND = 1000;
+  private static final int NO_FIND = 1000;
 
-    private Context mContext;
+  private Context mContext;
 
-    public RxErrorHandler(Context context) {
+  public RxErrorHandler(Context context) {
 
-        this.mContext = context;
+    this.mContext = context;
+  }
+
+  public BaseException handleError(Throwable e) throws IOException {
+    Log.v("RxError", e.toString());
+    BaseException exception = new BaseException();
+    if (e instanceof ApiException) {
+      exception.setCode(BaseException.API_ERROR);
+      exception.setDisplayMessage(ErrorMessageFactory.create(mContext, BaseException.API_ERROR));
+    } else if (e instanceof JsonParseException) {
+      exception.setCode(BaseException.JSON_ERROR);
+      exception.setDisplayMessage(ErrorMessageFactory.create(mContext, BaseException.JSON_ERROR));
+    } else if (e instanceof RxTransformerException) {
+      exception.setCode(((BaseException)e).getCode());
+      exception.setDisplayMessage(((BaseException)e).getDisplayMessage());
+    } else if (e instanceof SocketTimeoutException) {
+      exception.setCode(BaseException.SOCKET_TIMEOUT_ERROR);
+      exception.setDisplayMessage(
+          ErrorMessageFactory.create(mContext, BaseException.SOCKET_TIMEOUT_ERROR));
+    } else if (e instanceof UnknownHostException) {
+      exception.setCode(BaseException.UNKOWNHOST_ERROR);
+      exception.setDisplayMessage(
+          ErrorMessageFactory.create(mContext, BaseException.UNKOWNHOST_ERROR));
+    } else if (e instanceof SocketException) {
+      exception.setCode(BaseException.SOCKET_TIMEOUT_ERROR);
+      exception.setDisplayMessage(
+          ErrorMessageFactory.create(mContext, BaseException.SOCKET_TIMEOUT_ERROR));
+    } else if (e instanceof NetworkErrorException) {
+      exception.setCode(BaseException.SOCKET_ERROR);
+      exception.setDisplayMessage(ErrorMessageFactory.create(mContext, BaseException.SOCKET_ERROR));
+    } else if (e instanceof HttpException) {
+      exception.setCode(((HttpException) e).code());
+      HttpException httpException = (HttpException) e;
+      Gson gson = new Gson();
+      //后台服务器返回的错误json
+      Message messages = gson.fromJson(httpException.response().errorBody().string(),
+          Message.class);
+      if (!Strings.isNullOrEmpty(messages.getMessage())) {
+        exception.setDisplayMessage(messages.getMessage());
+      } else {
+        exception.setDisplayMessage(ErrorMessageFactory.create(mContext, exception.getCode()));
+      }
+    } else {
+      exception.setCode(BaseException.UNKNOWN_ERROR);
+      exception.setDisplayMessage(
+          ErrorMessageFactory.create(mContext, BaseException.UNKNOWN_ERROR));
     }
+    return exception;
+  }
 
-    public BaseException handleError(Throwable e) throws IOException {
-        Log.v("RxError", e.toString());
-        BaseException exception = new BaseException();
-        if (e instanceof ApiException) {
-            exception.setCode(BaseException.API_ERROR);
-            exception.setDisplayMessage(ErrorMessageFactory.create(mContext, BaseException.API_ERROR));
-        } else if (e instanceof JsonParseException) {
-            exception.setCode(BaseException.JSON_ERROR);
-            exception.setDisplayMessage(ErrorMessageFactory.create(mContext, BaseException.JSON_ERROR));
-        } else if (e instanceof RxTransformerException) {
-            exception.setCode(((BaseException) e).getCode());
-            exception.setDisplayMessage(((BaseException) e).getDisplayMessage());
-        } else if (e instanceof SocketTimeoutException) {
-            exception.setCode(BaseException.SOCKET_TIMEOUT_ERROR);
-            exception.setDisplayMessage(
-                ErrorMessageFactory.create(mContext, BaseException.SOCKET_TIMEOUT_ERROR));
-        } else if (e instanceof UnknownHostException) {
-            exception.setCode(BaseException.UNKOWNHOST_ERROR);
-            exception.setDisplayMessage(
-                ErrorMessageFactory.create(mContext, BaseException.UNKOWNHOST_ERROR));
-        } else if (e instanceof SocketException) {
-            exception.setCode(BaseException.SOCKET_TIMEOUT_ERROR);
-            exception.setDisplayMessage(
-                ErrorMessageFactory.create(mContext, BaseException.SOCKET_TIMEOUT_ERROR));
-        } else if (e instanceof NetworkErrorException) {
-            exception.setCode(BaseException.SOCKET_ERROR);
-            exception.setDisplayMessage(ErrorMessageFactory.create(mContext, BaseException.SOCKET_ERROR));
-        } else if (e instanceof HttpException) {
-            exception.setCode(((HttpException) e).code());
-            HttpException httpException = (HttpException) e;
-            Gson gson = new Gson();
-            if (httpException.response() != null
-                && httpException.response().errorBody() != null
-                && !Strings.isNullOrEmpty(httpException.response().errorBody().string())) {
-                //请求成功后后台服务器返回的错误json
-                exception.setDisplayMessage(gson.fromJson(httpException.response().errorBody().string(), Message.class).getMessage());
-            } else {
-                //请求发生的http错误
-                exception.setDisplayMessage(ErrorMessageFactory.create(mContext, exception.getCode()));
-            }
-        } else {
-            exception.setCode(BaseException.UNKNOWN_ERROR);
-            exception.setDisplayMessage(
-                ErrorMessageFactory.create(mContext, BaseException.UNKNOWN_ERROR));
-        }
-        return exception;
-    }
-
-    public void showErrorMessage(BaseException e) {
-        Toast.makeText(mContext, e.getDisplayMessage(), Toast.LENGTH_SHORT).show();
-    }
+  public void showErrorMessage(BaseException e) {
+    Toast.makeText(mContext, e.getDisplayMessage(), Toast.LENGTH_SHORT).show();
+  }
 }
